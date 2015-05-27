fab0a8d73d8916effeefec8f0735ccd388438dc6
ditek <diaa.33@gmail.com>
2015-05-27 02:31:56 +0200

    Initial commit
    Working USB interface with eco functionality.

---
+++ USB_Interface/USB_Interface.c
@@ -0,0 +1,17 @@
+/*
+ * USB_Interface.c
+ *
+ * Created: 14/05/2015 18:31:55
+ *  Author: diaaj
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
---
+++ USB_Interface/src/Board/Board.h
@@ -0,0 +1,16 @@
+/*
+ * Board.h
+ *
+ * Created: 25/05/2015 14:36:00
+ *  Author: diaaj
+ */ 
+
+
+#ifndef BOARD_H_
+#define BOARD_H_
+
+
+
+
+
+#endif /* BOARD_H_ */
